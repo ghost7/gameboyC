cdba1f8b6e622c07438433c0fd22e0e9aa4ed201
ghost7 <ghost7dev@gmail.com>
2012-07-12 00:43:48 -0700

    Created temporary MemoryEmulator until memory is complete.
    Signed-off-by: ghost7 <ghost7game@gmail.com>

--- src/gameboy.cpp
+++ src/gameboy.cpp
@@ -12,6 +12,37 @@
 
 #include "Window/GBWindow.h"
 #include "Window/GBSDLWindow.h"
+#include <stdint.h>
+
+#define MEM_SIZE 0xFFFF
+
+class MemoryEmulator : public MemoryInterface
+{
+public:
+    uint8_t read(addr_t memAddr)
+    {
+        return memory[memAddr];
+    }
+    void write(addr_t memAddr, data_t data)
+    {
+        memory[memAddr] = data;
+    }
+    data_t *getMemoryLocation(addr_t addr)
+    {
+        return &memory[addr];
+    }
+
+    void clearMemory()
+    {
+        memset(memory, 0, MEM_SIZE);
+    }
+    MemoryEmulator()
+    {
+        clearMemory();
+    }
+private:
+    data_t memory[MEM_SIZE];
+};
 
 static void usage()
 {
@@ -32,7 +63,7 @@ int main(int argc, char **argv)
     std::cout << mem->header->desc << std::endl;
     
     // Create Game Boy CPU.
-    CpuBase *cpu = new Z80Cpu(mem);
+    CpuBase *cpu = new Z80Cpu(new MemoryEmulator());
     cpu->init();
     
     // Create the window.
